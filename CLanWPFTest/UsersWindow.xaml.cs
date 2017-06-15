35ab0627130e1918b304ff7140865f0a81f556e4
ezgiakcora <ezgiakcora@gmail.com>
2017-06-15 21:08:07 +0200

    Small bug fixes

--- CLanWPFTest/UsersWindow.xaml.cs
+++ CLanWPFTest/UsersWindow.xaml.cs
@@ -17,33 +17,34 @@ namespace CLanWPFTest
     public partial class UsersWindow : Page
     {
         public string toSend = null;
-        
-        public UsersWindow(string fileToSend = null) {
+
+        public UsersWindow(string fileToSend = null)
+        {
             InitializeComponent();
             this.DataContext = this;
             this.toSend = fileToSend;
             this._continue.IsEnabled = false;    // Disable the "send" button until a user is selected.
-        }    
+        }
         private void PrivateMode_Checked(object sender, RoutedEventArgs e)
         {
             CLanUDPManager.GoOffline();
             Console.WriteLine("private!");
         }
-        
+
         private void PublicMode_Checked(object sender, RoutedEventArgs e)
         {
             CLanUDPManager.GoOnline();
             Console.WriteLine("public!");
         }
-        
+
         private void continueClick(object sender, RoutedEventArgs e)
         {
             List<User> users = new List<User>();
             users.Add(UserList.SelectedItem as User);
-            if(users.Count > 0)
+            if (users.Count > 0)
                 this._continue.IsEnabled = true;    // Enable the "send" button if at least one user is selected.
             this.NavigationService.Navigate(new FileTransfer(toSend, users));
-                  
+
         }
 
         private void Settings_Click(object sender, RoutedEventArgs e)
